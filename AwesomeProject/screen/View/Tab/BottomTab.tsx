31bfb4b167ffa0698e7c2a5156b6fab9d5128b01
ruri <devruri@gmail.com>
2024-03-04 17:08:59 +0900

    BottomTab.tsx prettier설정 적용

--- AwesomeProject/screen/View/Tab/BottomTab.tsx
+++ AwesomeProject/screen/View/Tab/BottomTab.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {AppRegistry, Text, View, Image, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Text, Image} from 'react-native';
 import Home from '../Home';
 import Class from '../Class';
 import Post from '../Post';
@@ -9,89 +9,121 @@ import Profile from '../Profile';
 
 const Tabs = createBottomTabNavigator();
 
-export default function BottomTab({navigation, route} : any) {
+export default function BottomTab() {
   return (
     <Tabs.Navigator>
-      <Tabs.Screen name="홈" component={Home} options= {{
+      <Tabs.Screen
+        name="홈"
+        component={Home}
+        options={{
           tabBarLabel: ({focused, color}) => {
-            return (
-              <Text style={{color: focused ? 'black' : color}}>홈</Text>
-            )
+            return <Text style={{color: focused ? 'black' : color}}>홈</Text>;
           },
           // 탭 아이콘 설정
-          tabBarIcon : ({focused}) => {
+          tabBarIcon: ({focused}) => {
             return (
               <Image
-                source={focused ? require("../../../assets/Home_active.png") : require("../../../assets/Home_inactive.png")} 
-                style = {{width : 30, height : 30}}
-                />
-            )
-          }
-        }}/>
-      <Tabs.Screen name="모임" component={Class} options= {{
+                source={
+                  focused
+                    ? require('../../../assets/Home_active.png')
+                    : require('../../../assets/Home_inactive.png')
+                }
+                style={{width: 30, height: 30}}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="모임"
+        component={Class}
+        options={{
           tabBarLabel: ({focused, color}) => {
-            return (
-              <Text style={{color: focused ? 'black' : color}}>모임</Text>
-            )
+            return <Text style={{color: focused ? 'black' : color}}>모임</Text>;
           },
           // 탭 아이콘 설정
-          tabBarIcon : ({focused}) => {
+          tabBarIcon: ({focused}) => {
             return (
               <Image
-                source={focused ? require("../../../assets/Class_active.png") : require("../../../assets/Class_inactive.png")} 
-                style = {{width : 30, height : 30}}
-                />
-            )
-          }
-        }}/>
-      <Tabs.Screen name="등록" component={Post} options= {{
+                source={
+                  focused
+                    ? require('../../../assets/Class_active.png')
+                    : require('../../../assets/Class_inactive.png')
+                }
+                style={{width: 30, height: 30}}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="등록"
+        component={Post}
+        options={{
           tabBarLabel: ({focused, color}) => {
-            return (
-              <Text style={{color: focused ? 'black' : color}}>등록</Text>
-            )
+            return <Text style={{color: focused ? 'black' : color}}>등록</Text>;
           },
           // 탭 아이콘 설정
-          tabBarIcon : ({focused}) => {
+          tabBarIcon: ({focused}) => {
             return (
               <Image
-                source={focused ? require("../../../assets/Post_active.png") : require("../../../assets/Post_inactive.png")} 
-                style = {{width : 30, height : 30}}
-                />
-            )
-          }
-        }}/>
-      <Tabs.Screen name="채팅" component={Chat} options= {{
+                source={
+                  focused
+                    ? require('../../../assets/Post_active.png')
+                    : require('../../../assets/Post_inactive.png')
+                }
+                style={{width: 30, height: 30}}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="채팅"
+        component={Chat}
+        options={{
           tabBarLabel: ({focused, color}) => {
-            return (
-              <Text style={{color: focused ? 'black' : color}}>채팅</Text>
-            )
+            return <Text style={{color: focused ? 'black' : color}}>채팅</Text>;
           },
           // 탭 아이콘 설정
-          tabBarIcon : ({focused}) => {
+          tabBarIcon: ({focused}) => {
             return (
               <Image
-                source={focused ? require("../../../assets/Chat_active.png") : require("../../../assets/Chat_inactive.png")} 
-                style = {{width : 30, height : 30}}
-                />
-            )
-          }
-        }}/>
-      <Tabs.Screen name="프로필" component={Profile} options= {{
+                source={
+                  focused
+                    ? require('../../../assets/Chat_active.png')
+                    : require('../../../assets/Chat_inactive.png')
+                }
+                style={{width: 30, height: 30}}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="프로필"
+        component={Profile}
+        options={{
           tabBarLabel: ({focused, color}) => {
             return (
               <Text style={{color: focused ? 'black' : color}}>프로필</Text>
-            )
+            );
           },
           // 탭 아이콘 설정
-          tabBarIcon : ({focused}) => {
+          tabBarIcon: ({focused}) => {
             return (
               <Image
-                source={focused ? require("../../../assets/Profile_active.png") : require("../../../assets/Profile_inactive.png")} 
-                style = {{width : 30, height : 30}}
-                />
-            )
-          }
-        }}/>
+                source={
+                  focused
+                    ? require('../../../assets/Profile_active.png')
+                    : require('../../../assets/Profile_inactive.png')
+                }
+                style={{width: 30, height: 30}}
+              />
+            );
+          },
+        }}
+      />
     </Tabs.Navigator>
   );
 }
